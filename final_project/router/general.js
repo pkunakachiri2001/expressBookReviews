b7c9af3df2d55a04f478c42c296d4b7a42981c2d
panashe <kunaka2001@gmail.com>
2026-09-10 09:03:21 -0400

    Added Axios async functions for Tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -100,3 +100,47 @@ public_users.get('/review/:isbn', function (req, res) {
 });
 
 module.exports.general = public_users;
+const axios = require('axios');
+
+// Task 10: Get all books using async/await with Axios
+public_users.get('/async-books', async function (req, res) {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({message: "Error fetching books"});
+    }
+});
+
+// Task 11: Get book details by ISBN using async/await with Axios
+public_users.get('/async-isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({message: "Error fetching book"});
+    }
+});
+
+// Task 12: Get book details by Author using async/await with Axios
+public_users.get('/async-author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({message: "Error fetching by author"});
+    }
+});
+
+// Task 13: Get book details by Title using async/await with Axios
+public_users.get('/async-title/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({message: "Error fetching by title"});
+    }
+});
